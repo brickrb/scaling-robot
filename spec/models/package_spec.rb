05d632ba5917f2d3e77050753bd6629b6462a13d
maclover7 <me@jonathanmoss.me>
2015-06-14 13:42:01 -0400

    Package model validations

--- spec/models/package_spec.rb
+++ spec/models/package_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Package, type: :model do
   it "has a valid factory" do
     FactoryGirl.build(:package).should be_valid
   end
+
+  it "invalid without a name" do
+    FactoryGirl.build(:package, name: nil).should_not be_valid
+  end
+
+  it "invalid with a duplicate name" do
+    FactoryGirl.create(:package, name: "rails")
+    FactoryGirl.build(:package, name: "rails").should_not be_valid
+  end
 end
