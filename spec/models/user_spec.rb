471e6dd7e425f2a0dfcc3f917f92771f8b2b7141
maclover7 <me@jonathanmoss.me>
2015-06-15 18:10:04 -0400

    Make sure users have usernames and also make sure they are unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     FactoryGirl.build(:user).should be_valid
   end
+
+  it "invalid without a username" do
+    FactoryGirl.build(:user, username: nil).should_not be_valid
+  end
+
+  it "invalid with a duplicate username" do
+    FactoryGirl.create(:user, username: "test")
+    FactoryGirl.build(:user, username: "test").should_not be_valid
+  end
 end
