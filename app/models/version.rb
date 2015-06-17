be3602afdcc3323e8cc211d22c83e3f29d77d528
maclover7 <me@jonathanmoss.me>
2015-06-17 10:27:39 -0400

    Version model validations

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,3 +1,7 @@
 class Version < ActiveRecord::Base
   belongs_to :package
+
+  validates :license, presence: true
+  validates :number, presence: true
+  validates :number, uniqueness: true
 end
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Version, type: :model do
   end
 
   it { should belong_to(:package) }
+
+  it "invalid without a license" do
+    FactoryGirl.build(:version, license: nil).should_not be_valid
+  end
+
+  it "invalid without a number" do
+    FactoryGirl.build(:version, number: nil).should_not be_valid
+  end
+
+  it "invalid with a duplicate number" do
+    FactoryGirl.create(:version, number: "1.0.0")
+    FactoryGirl.build(:version, number: "1.0.0").should_not be_valid
+  end
 end
